f24e122bb4d84a842399074a1ffe87574cd46332
Giuliano Bertoti <giuliano.bertoti@gmail.com>
2018-04-02 09:40:58 -0300

    update

---
+++ BotHeroku/src/main/java/br/com/simnetwork/BotByCasseb/BotByCassebApplication.java
@@ -0,0 +1,14 @@
+package br.com.simnetwork.BotByCasseb;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotByCassebApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(BotByCassebApplication.class, args);
+	}
+	
+}
